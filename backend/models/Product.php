931b5f97672dfcc8ad52e10fb45f480c2167f6ef
Joan0o <shuan779@gmail.com>
2025-08-05 20:06:33 -0500

    first commit

---
+++ backend/models/Product.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace backend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "product".
+ *
+ * @property int $id
+ * @property int|null $category_id
+ * @property int|null $subcategory_id
+ * @property string $name
+ *
+ * @property Category $category
+ * @property Subcategory $subcategory
+ */
+class Product extends \yii\db\ActiveRecord
+{
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'product';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['category_id', 'subcategory_id'], 'default', 'value' => null],
+            [['category_id', 'subcategory_id'], 'integer'],
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 100],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::class, 'targetAttribute' => ['category_id' => 'id']],
+            [['subcategory_id'], 'exist', 'skipOnError' => true, 'targetClass' => Subcategory::class, 'targetAttribute' => ['subcategory_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_id' => 'Category ID',
+            'subcategory_id' => 'Subcategory ID',
+            'name' => 'Name',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::class, ['id' => 'category_id']);
+    }
+
+    /**
+     * Gets query for [[Subcategory]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSubcategory()
+    {
+        return $this->hasOne(Subcategory::class, ['id' => 'subcategory_id']);
+    }
+
+}
---
+++ common/models/Subcategory.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "subcategory".
+ *
+ * @property int $id
+ * @property int|null $category_id
+ * @property string $name
+ * @property string|null $status
+ *
+ * @property Category $category
+ * @property Product[] $products
+ */
+class Subcategory extends \yii\db\ActiveRecord
+{
+
+    /**
+     * ENUM field values
+     */
+    const STATUS_ACTIVE = 'active';
+    const STATUS_INACTIVE = 'inactive';
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'subcategory';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['category_id'], 'default', 'value' => null],
+            [['status'], 'default', 'value' => 'active'],
+            [['category_id'], 'integer'],
+            [['name'], 'required'],
+            [['status'], 'string'],
+            [['name'], 'string', 'max' => 100],
+            ['status', 'in', 'range' => array_keys(self::optsStatus())],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::class, 'targetAttribute' => ['category_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'category_id' => 'Category ID',
+            'name' => 'Name',
+            'status' => 'Status',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::class, ['id' => 'category_id']);
+    }
+
+    /**
+     * Gets query for [[Products]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getProducts()
+    {
+        return $this->hasMany(Product::class, ['subcategory_id' => 'id']);
+    }
+
+
+    /**
+     * column status ENUM value labels
+     * @return string[]
+     */
+    public static function optsStatus()
+    {
+        return [
+            self::STATUS_ACTIVE => 'active',
+            self::STATUS_INACTIVE => 'inactive',
+        ];
+    }
+
+    /**
+     * @return string
+     */
+    public function displayStatus()
+    {
+        return self::optsStatus()[$this->status];
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStatusActive()
+    {
+        return $this->status === self::STATUS_ACTIVE;
+    }
+
+    public function setStatusToActive()
+    {
+        $this->status = self::STATUS_ACTIVE;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isStatusInactive()
+    {
+        return $this->status === self::STATUS_INACTIVE;
+    }
+
+    public function setStatusToInactive()
+    {
+        $this->status = self::STATUS_INACTIVE;
+    }
+}
